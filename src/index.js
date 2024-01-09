ae0ca266b7c9f54b4154afa2e318ac4878b7eea3
Vladimír Gorej <vladimir.gorej@gmail.com>
2024-01-09 09:42:30 +0100

    chore: integrate swagger-ui with React@18 support (#2)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const rootNode = document.getElementById('root');
-ReactDOM.render(<App />, rootNode);
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
